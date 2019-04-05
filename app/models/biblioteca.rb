8ce2c6f74d802f045fa99a440397b97763a8773c
AndersonNesi <nesi.anderson@outlook.com>
2019-04-04 22:16:59 -0300

    testeconfigurado

--- app/models/biblioteca.rb
+++ app/models/biblioteca.rb
@@ -2,5 +2,5 @@ class Biblioteca < ApplicationRecord
   belongs_to :usuario
   belongs_to :jogo
 
-  delegate :titulo, to: :jogo, prefix: false
+  delegate :titulo, to: :jogo, prefix: false         
 end
