b0266cb9ea8360987a4262f3fdc5b13a7900c814
AndersonNesi <hobold.edu@hotmail.com>
2019-04-04 21:38:36 -0300

    exit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :generos
+  resources :classificacoes
+  resources :avaliacaos
+  resources :jogos
+  resources :classificacaos
+  resources :categoria
+  resources :usuarios
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
